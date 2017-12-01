12fe2c7e1697f6abad80f5efc3a4735dda53d500
gegeliu <llwei@pku.edu.cn>
2017-12-01 15:49:14 +0800

    aiReply 增加event 消息类型支持

--- addons/aiReply/controller/Api.php
+++ addons/aiReply/controller/Api.php
@@ -18,12 +18,16 @@ class Api
             '充值'=>2,
             '免费'=>3,
             '帮助'=>4,
+            '验证码'=>5,
+            'code'=>5,
             );
 
     public function getKeyType( $txt )
     {
         $sys = self::$sys_keys;
-        if( strlen($txt) < 2 ) {
+        if( strlen($txt) < 2 || strlen($txt) > 15 ) {
+            if( '?' === $txt )
+                return 4;
             return 0;
         }
         foreach( $sys as $k => $v ) {
@@ -32,6 +36,28 @@ class Api
         }
         return 0;
     }
+
+    public function genVerifyCode($len)
+    {
+        $chars_array = array(
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+            'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        );
+        $charsLen = count($chars_array) - 1;
+
+        $outputstr = '';
+        for( $i=0; $i<$len; $i++ )
+        {
+            $outputstr .= $chars_array[mt_rand(0, $charsLen)];
+        }
+
+        return $outputstr;
+    }
+
 
     public function message($msg = [], $param = [])
     {
@@ -61,27 +87,63 @@ class Api
         }
 
         $nick = $usrinfo['nickname'];
+        $sex = $usrinfo['sex'];
+        $avatar = $usrinfo['headimgurl'];
         if (!empty($unionid)) {
             //查询 并更新用户昵称
-            $res = $db->table('user')->where('weixinopenid', $unionid)->count();
+            $res = $db->table('user')->where('weixinopenid', $unionid)->field('iduser,nickname,avatar')->find();
             if ($res) {
-
-                $avatar = $usrinfo['headimgurl'];
-                $db->table('user')->where('weixinopenid', $unionid)->update(['nickname' => $nick, 'avatar' => $avatar]);
+                $iduser = $res['iduser'];
+                if( $res['nickname'] !== $nick || $res['avatar'] !== $avatar ) {
+                    $db->table('user')->where('iduser', $iduser)->update(['nickname' => $nick, 'avatar' => $avatar, 'gender' => $sex]);
+                }
+            }
+            // 生成新用户
+            else {
+                $data = ['nickname'=>$nick,
+                        'gender'=>$sex,
+                        'weixinopenid'=>$unionid,
+                        'avatar'=>$avatar
+                        ];
+                $db->table('user')->insert($data);
             }
         }
         //
-        if ('text' !== $msg['MsgType']) {
+        if( 'event' === $msg['MsgType'] ){
+            $keyword = $msg['EventKey'];
+        }
+        else if ('text' === $msg['MsgType']) {
+            $keyword = $msg['Content'];
+        }
+        else {
             replyText('亲，暂不支持此类型消息哦...');
             return;
         }
         $books = array();
-        $keyword = $msg['Content'];
         if (is_numeric($keyword)) {
             $res = $db->table('novel')->where('idnovel', $keyword)->column('idnovel,name,cover_image');
         }
         else {
             $t = $this->getKeyType($keyword);
+            if( 5 === $t ){
+                $res = $db->table('user')->where('weixinopenid', $unionid)->field('iduser,name')->find();
+                if ($res && $res['iduser']){
+                    $iduser = $res['iduser'];
+                    $rand = $this->genVerifyCode(6);
+                    $code = substr(md5($iduser.$rand),8,16);
+                    $send_time = date('Y-m-d H:i:s', time());
+                    $data =['mobile_verify_state'=>3,
+                            'mobile_verify_code'=>$code,
+                            'mobile_verify_code_send_time'=>$send_time
+                            ];
+                    $db->table('user')->where('iduser',$iduser)->update($data);
+                    replyText($code);
+                }
+                else {
+                    replyText('验证码获取失败，请稍后再试。真是见了鬼了。');
+                }
+                return;
+            }
             if( 1 === $t ){ //最近阅读
                 $info = array (
                     'Title' => $keyword.'?',
@@ -91,7 +153,7 @@ class Api
                 );
                 array_push ( $books, $info );
             }
-            elseif( 2 === $t ){ // 充值
+            else if( 2 === $t ){ // 充值
                 $info = array ('Title' => $keyword.'?',
                     'Description' => '点我直接充值，感谢您的支持，我们一直在努力！',
                     'PicUrl' => '',
